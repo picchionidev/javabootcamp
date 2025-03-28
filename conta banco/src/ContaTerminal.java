9260eb50094b13c02fd0106bdd855fbdfdb2dfc6
Henrique Picchioni <henriquepicchioni16@gmail.com>
2025-03-28 14:08:30 -0300

    vf

--- conta banco/src/ContaTerminal.java
+++ conta banco/src/ContaTerminal.java
@@ -10,12 +10,15 @@ public class ContaTerminal {
 
         System.out.println("Digite o Número da conta:");
             numero=input.nextInt();
+            input.nextLine();
         System.out.println("Digite o Número da agência:");
             agencia=input.nextLine();
         System.out.println("Digite o Nome do Cliente:");
             nomeCliente=input.nextLine();
         System.out.println("Digite o Saldo da conta:");
             saldo=input.nextFloat();
+        
+        input.close();
 
         System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco! Sua agência é %s, conta %d e seu saldo %.2f Reais já está disponível para saque.", nomeCliente, agencia, numero, saldo));
 
